81750be185e8656558d543477a094a2c57f7377c
Øyvind Kolås <pippin@gimp.org>
2014-09-22 19:14:49 +0200

    add more files

---
+++ bin/mmfb-evsource-ts.c
@@ -0,0 +1,222 @@
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <linux/fb.h>
+#include <linux/input.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "mmfb-evsource.h"
+
+/* written to work with the zforce ir touchscreen of a kobo glo,
+ * probably works with a range of /dev/input/event classs of
+ * touchscreens.
+ */
+
+typedef struct Ts
+{
+  int    fd;
+  double  x;
+  double  y;
+  int    down;
+  int    rotate;
+  int    width;
+  int    height;
+} Ts;
+
+static Ts ts;
+static Ts* this = &ts;
+
+extern void *_mrg_evsrc_coord;
+
+static int read_sys_int (const char *path)
+{
+  int ret = -1;
+  int fd = open (path, O_RDONLY);
+  char buf[16]="";
+  if (fd>0)
+    {
+      if (read (fd, buf, 16)>0)
+        ret = atoi (buf);
+      close (fd);
+    }
+  return ret;
+}
+
+
+static int read_sys_int2 (const char *path, const char c)
+{
+  int ret = -1;
+  int fd = open (path, O_RDONLY);
+  char buf[32]="";
+  if (fd>0)
+    {
+      if (read (fd, buf, 32)>0)
+        {
+          char *p = strchr (buf, c);
+          if (p)
+            ret = atoi (p+1);
+        }
+      close (fd);
+    }
+  return ret;
+}
+
+static int ufb_evsource_ts_init ()
+{
+  /* need to detect which event */
+
+  this->down = 0;
+  this->fd = open ("/dev/input/event1", O_RDONLY | O_NONBLOCK);
+
+  this->rotate = read_sys_int ("/sys/class/graphics/fb0/rotate");
+  if (this->rotate < 0)
+    this->rotate = 0;
+  if (this->rotate)
+    {
+      this->width = read_sys_int2 ("/sys/class/graphics/fb0/modes", ':');
+      this->height = read_sys_int2 ("/sys/class/graphics/fb0/modes", 'x');
+    }
+
+  if (this->fd == -1)
+  {
+    fprintf (stderr, "error opening zforce device\n");
+    //sleep (1);
+    return -1;
+  }
+  _mrg_evsrc_coord = this;
+  return 0;
+}
+
+static void destroy ()
+{
+  if (this->fd != -1)
+    close (this->fd);
+}
+
+static int has_event ()
+{
+  struct timeval tv;
+  int retval;
+
+  if (this->fd == -1)
+    return 0;
+
+  fd_set rfds;
+  FD_ZERO (&rfds);
+  FD_SET(this->fd, &rfds);
+  tv.tv_sec = 0; tv.tv_usec = 0;
+  retval = select (this->fd+1, &rfds, NULL, NULL, &tv);
+  if (retval == 1)
+    return FD_ISSET (this->fd, &rfds);
+  return 0;
+}
+
+static char *get_event ()
+{
+  const char *ret = "mouse-motion";
+  struct input_event ev;
+  int buttstate = 0;
+  int sync = 0;
+
+  while (!sync){
+    int rc = read (this->fd, &ev, sizeof (ev));
+    if (rc != sizeof (ev))
+    {
+      //fprintf (stderr, "zforce read fail\n");
+      return NULL;
+    }
+    else
+    {
+      switch (ev.type)
+      {
+        case 3:
+          switch (ev.code)
+            {
+              case 0: this->x = ev.value; break;
+              case 1: this->y = ev.value; break;
+              default: break;
+            }
+          break;
+        case 1:
+          switch (ev.code)
+          {
+            case 0x014a:
+              switch (ev.value)
+                {
+                  case 0: ret = "mouse-release";
+                          this->down = 0;
+                     break;
+                  case 1: ret = "mouse-press";
+                          this->down = 1;
+                     break;
+                }
+              buttstate = 1;
+              break;
+          }
+          break;
+        case 0: /* sync (end of packet) */
+          sync = 1;
+          break;
+      }
+    }
+  }
+
+  if (!buttstate && this->down)
+    ret = "mouse-drag";
+
+  {
+    char *r = malloc (64);
+    switch (this->rotate)
+    {
+      case 1:
+        sprintf (r, "%s %.0f %.0f", ret, this->y, this->height-this->x);
+        break;
+      case 2:
+        sprintf (r, "%s %.0f %.0f", ret, this->height-this->x, this->width-this->y);
+        break;
+      case 3:
+        sprintf (r, "%s %.0f %.0f", ret, this->height-this->y, this->x);
+        break;
+      case 0:
+      default:
+        sprintf (r, "%s %.0f %.0f", ret, this->x, this->y);
+        break;
+    }
+    return r;
+  }
+
+  return NULL;
+}
+
+static int get_fd (EvSource *ev_source)
+{
+  return this->fd;
+}
+
+static void set_coord (EvSource *ev_source, double x, double y)
+{
+  fprintf (stderr, "can't really warp on a touch screen..\n");
+}
+
+static EvSource src = {
+  NULL,
+  (void*)has_event,
+  (void*)get_event,
+  (void*)destroy,
+  get_fd,
+  set_coord
+};
+
+EvSource *evsource_ts_new (void)
+{
+  if (ufb_evsource_ts_init () == 0)
+    {
+      return &src;
+    }
+  return NULL;
+}
---
+++ lib/mmfb-pset.h
@@ -0,0 +1,205 @@
+/* pset */
+
+#ifndef MMFB_PSET_H
+#define MMFB_PSET_H
+
+#include <math.h>
+
+ /* dithered optimized, partial accessible due to inlined factorization
+  */
+
+#ifndef  CLAMP
+#define CLAMP(num, min, max) {if (num < min) num = min;if (num > max)num=max;}
+#endif
+
+extern int eink_is_mono;
+
+static inline int a_dither_trc (int input)
+{
+  static int inited = 0;
+  static int trc[1024];
+  if (!inited)
+  {
+    int i;
+    inited = 1;
+    for (i = 0; i< 1024; i++)
+      trc[i] = round(pow (i / 1023.0, 0.75) * 1023.999);
+  }
+  if (input < 0) input = 0;
+  if (input > 1023) input = 1023;
+  return trc[input];
+}
+
+static inline int a_dither(int x, int y)
+{
+  return ((x ^ y * 237) * 181 & 511)/2.00;
+  //return ((x+    (y+(y<<2)+(y<<3)+(y<<8)))*3213) & 0xff;
+}
+
+static inline void ufb_dither_mono (int x, int y, 
+                                    int *red,
+                                    int *green,
+                                    int *blue)
+{
+  int dither = a_dither(x,y);
+  int value = a_dither_trc (*red + *green * 2.5 + *blue / 2) ;
+  value = value <= dither ? 0 : 255;
+  if(red)*red = value;
+  if (red == green) /* quick bail, when red==green==blue it indicates gray*/
+                    /* scale conversion,
+                     * XXX: need to special case this for this case.. `*/
+    return;
+  if(green)*green = value;
+  if(blue)*blue = value; 
+}
+
+static inline void ufb_dither_rgb (int x, int y, 
+                                   int *red,
+                                   int *green,
+                                   int *blue)
+{
+  int dither = a_dither(x,y);
+  dither = (dither >> 3) - 16;
+  *red = a_dither_trc (*red * 4) + dither;
+  CLAMP(*red, 0, 255);
+
+  //*red = a_dither(x,y);
+  if (red == green) /* quick bail, when red==green==blue it indicates gray*/
+                    /* scale conversion */
+    return;
+
+  *green = a_dither_trc (*green * 4) + dither;
+  CLAMP(*green, 0, 255);
+  *blue = a_dither_trc (*blue * 4) + dither;
+  CLAMP(*blue, 0, 255);
+}
+
+static inline void ufb_dither_generic (int x, int y, 
+                                       int *red,
+                                       int *green,
+                                       int *blue)
+{
+  if (eink_is_mono)
+    ufb_dither_mono (x, y, red, green, blue);
+  else
+    ufb_dither_rgb (x, y, red, green, blue);
+}
+
+#undef CLAMP
+
+/* somewhat optimized pset routines; as well as unwrapped version that
+ * permits efficient looping.
+ */
+
+#define Yu8_SET(p,r,g,b,a) do{p[0] = ((r)+(g)+(b))/3;}while(0)
+#define RGBu565_SET(p,r,g,b,a) do{p[0] = (*((uint16_t*)(p)) = ((r) >> 3) + (((g)>>2) << 5) + ((b>> 3) << (5+6)));}while(0);
+#define RGBu8_SET(p,r,g,b,a) do{ p[0] = (r); p[1] = (g); p[2] = (b); }while(0)
+#define RGBAu8_SET(p,r,g,b,a) do{ p[0] = (r); p[1] = (g); p[2] = (b); p[3] = (a); }while(0)
+  
+
+inline static unsigned char *ufb_pix_pset_nodither  (
+    Ufb *fb, unsigned char *pix, int bpp, int x, int y,
+    int red, int green, int blue, int alpha)
+{
+  switch (bpp)
+  {
+    case 1: /* grayscale framebuffer */
+      Yu8_SET(pix,red,green,blue,alpha);
+      break;
+    case 2: /* 16bit frame buffer, R' u5 G' u6 B' u5 */
+      RGBu565_SET(pix,red,green,blue,alpha);
+      break;
+    case 3: /* 24bit frame buffer, R'G'B' u8 */
+      RGBu8_SET(pix,red,green,blue,alpha);
+      break;
+    case 4: /* 32bit frame buffer, R'G'B'A u8 */
+      RGBAu8_SET(pix,red,green,blue,alpha);
+      break;
+  }
+  return pix + bpp;
+}
+
+inline static unsigned char *ufb_pix_pset_mono  (
+    Ufb *fb, unsigned char *pix, int bpp, int x, int y,
+    int red, int green, int blue, int alpha)
+{
+  int mono = (red+green+blue)/3;
+  ufb_dither_mono (x, y, &mono, &mono, &mono);
+
+  switch (bpp)
+  {
+    case 1: /* grayscale framebuffer */
+      Yu8_SET(pix,mono,mono,mono,alpha);
+      break;
+    case 2: /* 16bit frame buffer, R' u5 G' u6 B' u5 */
+      RGBu565_SET(pix,mono,mono,mono,alpha);
+      break;
+    case 3: /* 24bit frame buffer, R'G'B' u8 */
+      RGBu8_SET(pix,mono,mono,mono,alpha);
+      break;
+    case 4: /* 32bit frame buffer, R'G'B'A u8 */
+      RGBAu8_SET(pix,mono,mono,mono,alpha);
+      break;
+  }
+  return pix + bpp;
+}
+
+#define UFB_PCM_BUFFER_SIZE  8192
+
+#include <stdio.h>
+
+inline static unsigned char *ufb_pix_pset (
+    Ufb *fb, unsigned char *pix, int bpp, int x, int y,
+    int red, int green, int blue, int alpha)
+{
+  //fprintf (stderr, "%i %i %i %i\n", x, y, ufb_get_width (fb), ufb_get_height (fb));
+  ufb_dither_rgb (x, y, &red, &green , &blue);
+
+  switch (bpp)
+  {
+    case 1: /* grayscale framebuffer */
+      Yu8_SET(pix,red,green,blue,alpha);
+      break;
+    case 2: /* 16bit frame buffer, R' u5 G' u6 B' u5 */
+      RGBu565_SET(pix,red,green,blue,alpha);
+      break;
+    case 3: /* 24bit frame buffer, R'G'B' u8 */
+      RGBu8_SET(pix,red,green,blue,alpha);
+      break;
+    case 4: /* 32bit frame buffer, R'G'B'A u8 */
+      RGBAu8_SET(pix,red,green,blue,alpha);
+      break;
+  }
+  return pix + bpp;
+}
+
+inline static unsigned char *ufb_get_pix (Ufb *fb, unsigned char *buffer, int x, int y)
+{
+  /* this works, because bpp is the first member of fb, quickly getting this
+   * in a generic function like 
+  this is worth the hack. */
+  int bpp =    ((int*)(fb))[0];
+  int stride = ((int*)(fb))[1];
+  unsigned char *pix = &buffer[y * stride + x * bpp];
+  return pix;
+}
+
+/* a sufficiently fast pixel setter
+ */
+inline static void ufb_pset (Ufb *fb, unsigned char *buffer, int x, int y, int red, int green, int blue, int alpha)
+{
+  int bpp =    ((int*)(fb))[0];
+  unsigned char *pix = ufb_get_pix (fb, buffer, x, y);
+  ufb_pix_pset (fb, pix, bpp, x, y, red, green, blue, alpha);
+}
+
+inline static void ufb_pset_mono (Ufb *fb, unsigned char *buffer, int x, int y, int red, int green, int blue, int alpha)
+{
+  int bpp =    ((int*)(fb))[0];
+  unsigned char *pix = ufb_get_pix (fb, buffer, x, y);
+  ufb_pix_pset_mono (fb, pix, bpp, x, y, red, green, blue, alpha);
+}
+
+#endif
+
+/********/
